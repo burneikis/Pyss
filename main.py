94026829b142648c7152f2c54486cff6312fd712
Alex Burneikis <alexburneikis@gmail.com>
2022-10-01 13:40:48 +1000

    Add method for viewing game's moves

--- main.py
+++ main.py
@@ -4,16 +4,23 @@ import chess
 
 board = chess.Board()
 
-while not board.is_game_over():
+def game(depth):
+    played_moves = []
+    while not board.is_game_over():
+        print(board)
+        position = evaluate(board)
+        if board.turn:
+            print(f'White to move, Position: {position}')
+        else:
+            print(f'Black to move, Position: {position}')
+
+        move = best_move(board, depth)
+        board.push(move)
+
+        played_moves.append(str(move))
+
     print(board)
-    position = evaluate(board)
-    if board.turn:
-        print(f'White to move, Position: {position}')
-    else:
-        print(f'Black to move, Position: {position}')
+    print(played_moves)
+    print(f'Game over, {board.result()}')
 
-    move = best_move(board, 4)
-    board.push(move)
-
-print(board)
-print(f'Game over, {board.result()}')
+game(2)
