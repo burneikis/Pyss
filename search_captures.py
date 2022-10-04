613b3f9e4c42ebe6e1a5c19a58a50a93cbaa41a5
Alex Burneikis <alexburneikis@gmail.com>
2022-10-04 11:52:20 +1100

    re-wrote capture search

--- search_captures.py
+++ search_captures.py
@@ -1,19 +1,36 @@
 from evaluate import evaluate
 
-def search_captures(board, alpha, beta):
-    evaluation = evaluate(board)
-    if evaluation >= beta:
-        return beta
-    alpha = max(alpha, evaluation)
-
+def get_captures(board):
+    captures = []
     for move in board.legal_moves:
         if board.is_capture(move):
+            captures.append(move)
+    return captures
+
+
+def minimax_cap(board, alpha, beta):
+    captures = get_captures(board)
+    if len(captures) == 0:
+        return evaluate(board)
+    if board.turn:
+        maxEval = -9999
+        for move in captures:
             board.push(move)
-            evaluation = search_captures(board, alpha, beta)
+            eval = minimax_cap(board, alpha, beta)
             board.pop()
-
-            if evaluation >= beta:
-                return beta
-            alpha = max(alpha, evaluation)
-
-    return alpha
+            maxEval = max(maxEval, eval)
+            alpha = max(alpha, eval)
+            if beta <= alpha:
+                break
+        return maxEval
+    else:
+        minEval = 9999
+        for move in captures:
+            board.push(move)
+            eval = minimax_cap(board, alpha, beta)
+            board.pop()
+            minEval = min(minEval, eval)
+            beta = min(beta, eval)
+            if beta <= alpha:
+                break
+        return minEval
