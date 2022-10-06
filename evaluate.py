0ebfdb282aad719c1b0b227e89904f16a037fa86
Alex Burneikis <alexburneikis@gmail.com>
2022-10-06 18:44:27 +1100

    added piece position table

--- evaluate.py
+++ evaluate.py
@@ -14,6 +14,73 @@ center_squares = [
     chess.D4, chess.E4
 ]
 
+pawntable = [
+    0, 0, 0, 0, 0, 0, 0, 0,
+    5, 10, 10, -20, -20, 10, 10, 5,
+    5, -5, -10, 0, 0, -10, -5, 5,
+    0, 0, 0, 20, 20, 0, 0, 0,
+    5, 5, 10, 25, 25, 10, 5, 5,
+    10, 10, 20, 30, 30, 20, 10, 10,
+    50, 50, 50, 50, 50, 50, 50, 50,
+    0, 0, 0, 0, 0, 0, 0, 0
+]
+
+knightstable = [
+    -50, -40, -30, -30, -30, -30, -40, -50,
+    -40, -20, 0, 5, 5, 0, -20, -40,
+    -30, 5, 10, 15, 15, 10, 5, -30,
+    -30, 0, 15, 20, 20, 15, 0, -30,
+    -30, 5, 15, 20, 20, 15, 5, -30,
+    -30, 0, 10, 15, 15, 10, 0, -30,
+    -40, -20, 0, 0, 0, 0, -20, -40,
+    -50, -40, -30, -30, -30, -30, -40, -50
+]
+
+bishopstable = [
+    -20, -10, -10, -10, -10, -10, -10, -20,
+    -10, 0, 0, 0, 0, 0, 0, -10,
+    -10, 0, 5, 10, 10, 5, 0, -10,
+    -10, 5, 5, 10, 10, 5, 5, -10,
+    -10, 0, 10, 10, 10, 10, 0, -10,
+    -10, 10, 10, 10, 10, 10, 10, -10,
+    -10, 5, 0, 0, 0, 0, 5, -10,
+    -20, -10, -10, -10, -10, -10, -10, -20
+]
+
+rookstable = [
+    0, 0, 0, 0, 0, 0, 0, 0,
+    5, 10, 10, 10, 10, 10, 10, 5,
+    -5, 0, 0, 0, 0, 0, 0, -5,
+    -5, 0, 0, 0, 0, 0, 0, -5,
+    -5, 0, 0, 0, 0, 0, 0, -5,
+    -5, 0, 0, 0, 0, 0, 0, -5,
+    -5, 0, 0, 0, 0, 0, 0, -5,
+    0, 0, 0, 5, 5, 0, 0, 0
+]
+
+queenstable = [
+    -20, -10, -10, -5, -5, -10, -10, -20,
+    -10, 0, 0, 0, 0, 0, 0, -10,
+    -10, 0, 5, 5, 5, 5, 0, -10,
+    -5, 0, 5, 5, 5, 5, 0, -5,
+    0, 0, 5, 5, 5, 5, 0, -5,
+    -10, 5, 5, 5, 5, 5, 0, -10,
+    -10, 0, 5, 0, 0, 0, 0, -10,
+    -20, -10, -10, -5, -5, -10, -10, -20
+]
+
+kingstable = [
+    20, 30, 10, 0, 0, 10, 30, 20,
+    20, 20, 0, 0, 0, 0, 20, 20,
+    -10, -20, -20, -20, -20, -20, -20, -10,
+    -20, -30, -30, -40, -40, -30, -30, -20,
+    -30, -40, -40, -50, -50, -40, -40, -30,
+    -30, -40, -40, -50, -50, -40, -40, -30,
+    -30, -40, -40, -50, -50, -40, -40, -30,
+    -30, -40, -40, -50, -50, -40, -40, -30
+]
+
+
 def evaluate(board):
     # Checkmate/stalemate
     if board.is_checkmate():
@@ -25,7 +92,7 @@ def evaluate(board):
     # Material
     material = 0
     for piece in board.piece_map().values():
-        #get piece value
+        # get piece value
         value = values[piece.piece_type]
         material += value if piece.color else -value
 
@@ -42,5 +109,23 @@ def evaluate(board):
         if board.piece_at(square):
             center_control += 10 if board.piece_at(square).color else -10
 
+    # Table Value
+    # this is very slow
+    table_value = 0
+    # for piece in board.piece_map():
+    #     white = board.piece_at(piece).color
+    #     if board.piece_at(piece).piece_type == chess.PAWN:
+    #         table_value += pawntable[piece] if white else -pawntable[chess.square_mirror(piece)]
+    #     elif board.piece_at(piece).piece_type == chess.KNIGHT:
+    #         table_value += knightstable[piece] if white else -knightstable[chess.square_mirror(piece)]
+    #     elif board.piece_at(piece).piece_type == chess.BISHOP:
+    #         table_value += bishopstable[piece] if white else -bishopstable[chess.square_mirror(piece)]
+    #     elif board.piece_at(piece).piece_type == chess.ROOK:
+    #         table_value += rookstable[piece] if white else -rookstable[chess.square_mirror(piece)]
+    #     elif board.piece_at(piece).piece_type == chess.QUEEN:
+    #         table_value += queenstable[piece] if white else -queenstable[chess.square_mirror(piece)]
+    #     elif board.piece_at(piece).piece_type == chess.KING:
+    #         table_value += kingstable[piece] if white else -kingstable[chess.square_mirror(piece)]
 
-    return material + mobility + center_control
+
+    return material + mobility + center_control + table_value 
