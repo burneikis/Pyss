0f8999929cf3295818181f5d6204a5f99e54f342
Alex Burneikis <alexburneikis@gmail.com>
2022-10-08 20:11:14 +1100

    changes to piece_tables and another idea of thread

--- evaluate.py
+++ evaluate.py
@@ -1,4 +1,5 @@
 import chess
+#from piece_tables import piece_square_tables
 
 values = {
     chess.PAWN: 100,
@@ -9,79 +10,10 @@ values = {
     chess.KING: 20000
 }
 
-center_squares = [
-    chess.D5, chess.E5,
-    chess.D4, chess.E4
-]
-
-pawntable = [
-    0, 0, 0, 0, 0, 0, 0, 0,
-    5, 10, 10, -20, -20, 10, 10, 5,
-    5, -5, -10, 0, 0, -10, -5, 5,
-    0, 0, 0, 20, 20, 0, 0, 0,
-    5, 5, 10, 25, 25, 10, 5, 5,
-    10, 10, 20, 30, 30, 20, 10, 10,
-    50, 50, 50, 50, 50, 50, 50, 50,
-    0, 0, 0, 0, 0, 0, 0, 0
-]
-
-knightstable = [
-    -50, -40, -30, -30, -30, -30, -40, -50,
-    -40, -20, 0, 5, 5, 0, -20, -40,
-    -30, 5, 10, 15, 15, 10, 5, -30,
-    -30, 0, 15, 20, 20, 15, 0, -30,
-    -30, 5, 15, 20, 20, 15, 5, -30,
-    -30, 0, 10, 15, 15, 10, 0, -30,
-    -40, -20, 0, 0, 0, 0, -20, -40,
-    -50, -40, -30, -30, -30, -30, -40, -50
-]
-
-bishopstable = [
-    -20, -10, -10, -10, -10, -10, -10, -20,
-    -10, 0, 0, 0, 0, 0, 0, -10,
-    -10, 0, 5, 10, 10, 5, 0, -10,
-    -10, 5, 5, 10, 10, 5, 5, -10,
-    -10, 0, 10, 10, 10, 10, 0, -10,
-    -10, 10, 10, 10, 10, 10, 10, -10,
-    -10, 5, 0, 0, 0, 0, 5, -10,
-    -20, -10, -10, -10, -10, -10, -10, -20
-]
-
-rookstable = [
-    0, 0, 0, 0, 0, 0, 0, 0,
-    5, 10, 10, 10, 10, 10, 10, 5,
-    -5, 0, 0, 0, 0, 0, 0, -5,
-    -5, 0, 0, 0, 0, 0, 0, -5,
-    -5, 0, 0, 0, 0, 0, 0, -5,
-    -5, 0, 0, 0, 0, 0, 0, -5,
-    -5, 0, 0, 0, 0, 0, 0, -5,
-    0, 0, 0, 5, 5, 0, 0, 0
-]
-
-queenstable = [
-    -20, -10, -10, -5, -5, -10, -10, -20,
-    -10, 0, 0, 0, 0, 0, 0, -10,
-    -10, 0, 5, 5, 5, 5, 0, -10,
-    -5, 0, 5, 5, 5, 5, 0, -5,
-    0, 0, 5, 5, 5, 5, 0, -5,
-    -10, 5, 5, 5, 5, 5, 0, -10,
-    -10, 0, 5, 0, 0, 0, 0, -10,
-    -20, -10, -10, -5, -5, -10, -10, -20
-]
-
-kingstable = [
-    20, 30, 10, 0, 0, 10, 30, 20,
-    20, 20, 0, 0, 0, 0, 20, 20,
-    -10, -20, -20, -20, -20, -20, -20, -10,
-    -20, -30, -30, -40, -40, -30, -30, -20,
-    -30, -40, -40, -50, -50, -40, -40, -30,
-    -30, -40, -40, -50, -50, -40, -40, -30,
-    -30, -40, -40, -50, -50, -40, -40, -30,
-    -30, -40, -40, -50, -50, -40, -40, -30
-]
-
-#have one empty index at the beginning so that the piece type can be used as an index directly
-piece_square_tables = [None, pawntable, knightstable, bishopstable, rookstable, queenstable, kingstable]
+# center_squares = [
+#     chess.D5, chess.E5,
+#     chess.D4, chess.E4
+# ]
 
 def evaluate(board):
     # Checkmate/stalemate
@@ -113,10 +45,10 @@ def evaluate(board):
 
     # Table Value
     # this is very slow
-    table_value = 0
-    for piece in board.piece_map():
-        white = board.piece_at(piece).color
-        piece_type = board.piece_at(piece).piece_type
-        table_value += piece_square_tables[piece_type][piece] if white else -piece_square_tables[piece_type][piece]
+    # table_value = 0
+    # for piece in board.piece_map():
+    #     white = board.piece_at(piece).color
+    #     piece_type = board.piece_at(piece).piece_type
+    #     table_value += piece_square_tables[piece_type][piece] if white else -piece_square_tables[piece_type][piece]
 
-    return material + table_value 
+    return material #+ mobility + center_control + table_value 
--- get_move.py
+++ get_move.py
@@ -1,10 +1,11 @@
 from minimax import minimax
+import threading
 
 def get_move(board, depth):
     best_move = None
     best_score = -9999
 
-    for move in board.legal_moves:
+    def thread_function(board, depth, move):
         board.push(move)
         score = minimax(board, depth - 1, -9999, 9999)
         board.pop()
@@ -12,8 +13,25 @@ def get_move(board, depth):
         if not board.turn:
             score *= -1
 
+        nonlocal best_score
+        nonlocal best_move
+
         if score >= best_score:
             best_score = score
-            best_move = move     
+            best_move = move
+
+    threads = []
+
+    for move in board.legal_moves:
+        board_copy = board.copy()
+
+        t = threading.Thread(target=thread_function, args=(board_copy, depth, move))
+        threads.append(t)
+
+    for thread in threads:
+        thread.start()
+
+    for thread in threads:
+        thread.join()
          
     return best_move
